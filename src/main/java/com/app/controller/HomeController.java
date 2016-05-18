fd0ef4abe12de54e5a96108fbbf502df992bec9c
Ashwin Oke <okeashwin@gmail.com>
2016-05-18 19:38:18 -0400

    Framework setup

---
+++ src/main/java/com/app/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.app.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/")
+	public ModelAndView test(HttpServletResponse response) throws ServletException, IOException {
+		return new ModelAndView("home");
+	}
+}
